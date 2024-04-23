fb855f0643be67b962997149306b40fd7e3de361
ThabangNWU <50927260+ThabangNWU@users.noreply.github.com>
2024-04-23 17:24:56 +0200

    Update CategoryRepository.java

--- src/main/java/com/thogwa/thogwa/backend/repository/CategoryRepository.java
+++ src/main/java/com/thogwa/thogwa/backend/repository/CategoryRepository.java
@@ -1,6 +1,9 @@
 package com.thogwa.thogwa.backend.repository;
 
 import com.thogwa.thogwa.backend.model.Category;
+import com.thogwa.thogwa.backend.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,4 +14,5 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
     Category findByCategoryName(String categoryName);
     List<Category> findByHeaderid(Integer headerId);
+    Page<Category> findAll(Pageable pageable);
 }
