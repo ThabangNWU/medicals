7f61413688612fe27bf3f6c923abd300fb846eb9
ThabangNWU <50927260+ThabangNWU@users.noreply.github.com>
2024-04-23 17:25:36 +0200

    Update HeaderCategoryRepository.java

--- src/main/java/com/thogwa/thogwa/backend/repository/HeaderCategoryRepository.java
+++ src/main/java/com/thogwa/thogwa/backend/repository/HeaderCategoryRepository.java
@@ -1,7 +1,3 @@
-package com.thogwa.thogwa.backend.repository;
-
-import com.thogwa.thogwa.backend.model.HeaderCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface HeaderCategoryRepository extends JpaRepository<HeaderCategory,Integer> {
+    Page<HeaderCategory> findAll(Pageable pageable);
 }
